db6869bcc43c6e1c9d1752a9c9981a8d2caf82fb
cjativa <cjativa@barnesfoundation.org>
2020-04-28 20:57:57 -0400

    1 file modified

--- UbuntuTerminal/commandHelper.ts
+++ UbuntuTerminal/commandHelper.ts
@@ -6,15 +6,33 @@ export const generateOutputForCommand = input => {
   switch (input) {
     case `ls`:
       return generateCommandHint(
-        `display the available folders and files within a directory. You would see something like this after running the command in an actual Ubuntu environment
+        `display the available folders and files within a directory. 
+        
+You would see something like this after running the command in an actual Ubuntu environment
+
 myFileOne.text myFileTwo.Text cat.png
-      `);
+      `
+      );
 
     case `mkdir`:
-      return generateCommandHint(`make a directory`);
+      return generateCommandHint(
+        `create a new directory
+
+You use the command along with the name you want to give the directory, like this
+
+mkdir MyPictures
+        `
+      );
 
     case `cp`:
-      return generateCommandHint(`copy a directory to another location`);
+      return generateCommandHint(
+        `copy a file or directory to another location
+
+You would use it like this to copy a file to a new location
+
+cp myAwesomeFile.txt MyDocuments
+        `
+      );
 
     default:
       return `Oops! I don't know about the "${input}"" command`;
